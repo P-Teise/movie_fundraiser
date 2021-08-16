ec79b760238008f2ebc452161d9eff7f1154c5c1
P-Teise <teumoh.teise@awatapu.school.nz>
2021-08-17 10:46:55 +1200

    ticket loop function - version 1. loops 5 times(testing)

---
+++ 02_ticket_loop_v1.py
@@ -0,0 +1,15 @@
+# loop function for tickets
+# start of loop
+
+name = ""
+count = 0
+max_tickets = 5
+
+while name != "xxx" and count < max_tickets:
+
+    # get details
+
+    name = input("Name: ")
+    count += 1
+
+
