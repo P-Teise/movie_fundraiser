246a97e7791e79522d87dd3b477778ea95ecb739
P-Teise <teumoh.teise@awatapu.school.nz>
2021-09-21 09:41:09 +1200

    added int_checker to base component w/ comments.

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -1,29 +1,80 @@
-
-
 # Functions go here
-
-    # check that ticket holder name is not blank
- def not_blank(question, error_message):
+# check that ticket holder name is not blank
+def not_blank(question, error_message):
     valid = False
 
     while not valid:
         response = input(question)
 
-    # if input is not blank, program continues
+        # if input is not blank, program continues
         if response != "":
             return response
+
         # if input is blank, display error message (& loop until there is correct input)
 
         else:
             print(error_message)
 
 
-# ******* Main routine ***********
+# checks for an integer between two values
 
-# <<<<<<< Loop to get ticket detail >>>>>>>>
+
+def int_check(question, low_num, high_num):
+    error = "Please enter a whole number between {} " \
+            "and {}".format(low_num, high_num)
+
+    valid = False
+    while not valid:
+
+        # ask user for number and check it is valid
+        try:
+            response = int(input(question))
+
+            if low_num <= response <= high_num:
+                return response
+            else:
+                print(error)
+
+        # if integer is not entered, display an error message
+        except ValueError:
+            print(error)
+
+    # ******* Main routine ***********
+
+    # <<<<<<< Loop to get ticket detail >>>>>>>>
+    # start of loop
+    # initialise loop so that it runs at least once
+
+    name = ""
+    count = 0
+    max_tickets = 5
+
+    while name != "xxx" and count < max_tickets:
+
+        # tells user how many seats are left
+        if count == max_tickets:
+            print("You have sold all the available tickets!")
+
+        # tells user how many tickets have been sold and how many tickets are left
+        elif count < 4:
+            print("You have sold {} tickets. \n"
+                  "There are {} places still available"
+                  .format(count, max_tickets - count))
+    # warns user only ONE seat is left
+    else:
+        print("You have sold 4 tickets. \n"
+              "You have 1 seat left")
+
+    # Get details...
 
     # Get name (can't be blank)
     name = not_blank("Name: ",
-                 "Sorry - this can't be blank, "
-                 "please enter your name")
+                     "Sorry - this can't be blank, "
+                     "please enter your name")
 
+    # Get age (between 12 and 130)
+    age = int_check("Age: ", 12, 130)
+
+# End of tickets loop
+
+# calculate profits etc.
