77672d7268daa24476d36396c45eefd1a6c9cc33
P-Teise <teumoh.teise@awatapu.school.nz>
2021-09-22 10:42:18 +1200

    indented code relevant to while loop so it works. added count += 1. #get details is down the bottom

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -39,41 +39,39 @@ def int_check(question, low_num, high_num):
         except ValueError:
             print(error)
 
-    # ******* Main routine ***********
 
-    # <<<<<<< Loop to get ticket detail >>>>>>>>
-    # start of loop
-    # initialise loop so that it runs at least once
+# ******* Main routine ***********
 
-    name = ""
-    count = 0
-    max_tickets = 5
+# <<<<<<< Loop to get ticket detail >>>>>>>>
+# start of loop
+# initialise loop so that it runs at least once
 
-    while name != "xxx" and count < max_tickets:
+# Get name (can't be blank)
 
-        # tells user how many seats are left
-        if count == max_tickets:
-            print("You have sold all the available tickets!")
+count = 0
+max_tickets = 5
+name =""
 
-        # tells user how many tickets have been sold and how many tickets are left
-        elif count < 4:
-            print("You have sold {} tickets. \n"
-                  "There are {} places still available"
-                  .format(count, max_tickets - count))
+while name != "xxx" and count < max_tickets:
+
+    # tells user how many seats are left
+    if count == max_tickets:
+        print("You have sold all the available tickets!")
+
+    # tells user how many tickets have been sold and how many tickets are left
+    elif count < max_tickets - 1:
+        print("You have sold {} tickets. There are {} places still available" .format(count, max_tickets - count))
     # warns user only ONE seat is left
     else:
-        print("You have sold 4 tickets. \n"
-              "You have 1 seat left")
+        print("You have 1 seat left")
 
     # Get details...
+    name = not_blank("Name: ", "Please enter your name")
 
-    # Get name (can't be blank)
-    name = not_blank("Name: ",
-                     "Sorry - this can't be blank, "
-                     "please enter your name")
-
-    # Get age (between 12 and 130)
+     # Get age (between 12 and 130)
     age = int_check("Age: ", 12, 130)
+
+    count += 1
 
 # End of tickets loop
 
